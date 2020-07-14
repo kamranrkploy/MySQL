499ee5141965b58d5e00325e2da0f80c948fff5b
kamranrk <kamran16alam@gmail.com>
2020-07-14 20:59:25 +0530

    modified Select.js and Created Select_Required.js

--- Select_Required.js
+++ Select_Required.js
@@ -15,7 +15,8 @@ con.connect( (err) => {
     if (err) throw `${err}`;
     console.log(x);
 
-    let sql = "SELECT * FROM INFO_CUSTOMERS"; //* mean 'all' , you are selecting all from table name as INFO_CUSTOMER
+     //so for a huge data where you have million of rows. it is not a good idea to select all but rather properties that are required
+    let sql = "SELECT id , Name FROM INFO_CUSTOMERS";// properties will be shown in sequence for example in our case first id and then Name.
 
       con.query(sql , (err , result , fields) => {
              if(err) throw `${err}`;
@@ -24,7 +25,7 @@ con.connect( (err) => {
            //so you can easily loop through the data using different loops. for now we have used for loop.
            for(i=0 ; i<result.length ; i++)
                     {  
-                     console.log(result[i].Name +` your email is `+ result[i].Email);
+                     console.log(result[i]);
                     }
                });
             });
